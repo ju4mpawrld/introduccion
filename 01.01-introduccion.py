91b9c8892212923018f259a1f5703accff05d5cb
Juan Pablo <juanpabloveracanales@gmail.com>
2023-12-18 15:44:36 -0300

    introduccion

---
+++ 01.01-introduccion.py
@@ -0,0 +1,6 @@
+# lo que quiero hacer aqui es imprimir una clase que diga class float.
+Numero = "3.21"  # asigno mi string
+Numero_decimal = float(Numero)  # con esto convierto el 'Numero' a 'numero decimal'
+print(
+    type(Numero_decimal)
+)  # aqui imprimo el numero_decimal ya que yo busco que me imprima: <class 'float'>
---
+++ 01.02-introduccion.py
@@ -0,0 +1,3 @@
+Numero = "89"  # mi numero
+NumeroEntero = int(Numero)  # lo transformo a integer en vez de str
+print(type(NumeroEntero))
